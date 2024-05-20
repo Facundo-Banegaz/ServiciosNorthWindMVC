3fc4d394b5b5cc6b5500b57a3a31776981f97ad8
Facundo-Banegaz <Banegaz@users.noreply.github.com>
2024-05-19 22:17:19 -0300

    subiendo cambios

---
+++ ServiciosNorthWindMVC/Controllers/ShipperController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiciosNorthWindMVC.Controllers
+{
+    public class ShipperController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ServiciosNorthWindMVC/Models/Shipper.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServiciosNorthWindMVC.Models
+{
+    public class Shipper
+    {
+        [Key]
+        public int ShipperID { get; set; }
+
+        [Required(ErrorMessage = "El Nombre es obligatorio!!")]
+        [StringLength(40, ErrorMessage = "Solo permite hasta 40 caracteres!!")]
+        public string CompanyName { get; set; }
+        [Required(ErrorMessage = "El Telefono es obligatorio!!")]
+        [StringLength(24, ErrorMessage = "Solo permite hasta 24 caracteres!!")]
+        public string Phone { get; set; }
+    }
+}
